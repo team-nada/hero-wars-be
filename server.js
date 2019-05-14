0652da35a0b83ffca515508e3322d3d259a14f69
Paula Thomas <paula9t9@gmail.com>
2019-05-14 13:08:16 -0700

    Quick push for testing

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 require('dotenv').config();
 
-
 // Libraries imported thought NPM
 const express = require('express');
 const cors = require('cors');
@@ -24,8 +23,6 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.err(err));
 
-const characterData = [];
-
 function Characters(obj) {
   this.name = obj.name
   this.intelligence = obj.powerstats.intelligence,
@@ -40,19 +37,18 @@ function Characters(obj) {
   this.affiliation = obj.connections.groupAffiliation,
   this.smallImageURL = obj.images.sm,
   this.largeImageURL = obj.images.lg
-  
-  // console.log(this);
+
 }
 
-Characters.prototype = {
-  store: function (){
-    console.log('Storing data');
-    const insertStatement = 'INSERT INTO characters ( name, intelligence, strength, speed, durability, power, combat, publisher, alignment, race, groupAffiliation, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);';
-    const values = [this.name, this.intelligence, this.strength, this.speed, this.durability, this.power, this.combat, this.publisher, this.alignment, this.race, this.affiliation, this.largeImageURL];
-    client.query(insertStatement, values);
-  }
+//Stores the current characters data in the database
+// Characters.prototype = {
+//   store: function (){
+//     const insertStatement = 'INSERT INTO characters ( name, intelligence, strength, speed, durability, power, combat, publisher, alignment, race, groupAffiliation, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);';
+//     const values = [this.name, this.intelligence, this.strength, this.speed, this.durability, this.power, this.combat, this.publisher, this.alignment, this.race, this.affiliation, this.largeImageURL];
+//     client.query(insertStatement, values);
+//   }
   
-}
+// }
 
 //Checks if the DB has data
 function dbHasData(){
@@ -74,7 +70,6 @@ function callApi(){
     .then(result => {
       result.body.map((element) => {
         let currentCharacter = new Characters(element);
-        // console.log('currentCharacter: ', currentCharacter);
         currentCharacter.store();
       });
     }).catch(console.error(error));
